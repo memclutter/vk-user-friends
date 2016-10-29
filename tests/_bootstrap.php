576a6f213f5125fc1e290b1795015a4478f7c916
Memory Clutter <memclutter@gmail.com>
2016-10-29 21:27:44 +0300

    Fix test _bootstrap.php

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -4,3 +4,6 @@ defined('YII_DEBUG') or define('YII_DEBUG', true);
 
 require_once(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 require __DIR__ .'/../vendor/autoload.php';
+
+$dotenv = new \Dotenv\Dotenv(dirname(__DIR__));
+$dotenv->load();
