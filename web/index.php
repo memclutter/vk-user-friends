d3c192660f946680436dffa8d9b808c38184a8e4
Memory Clutter <memclutter@gmail.com>
2016-10-30 03:49:51 +0300

    Detect heroku app

--- web/index.php
+++ web/index.php
@@ -3,12 +3,15 @@
 // comment out the following two lines when deployed to production
 defined('YII_DEBUG') or define('YII_DEBUG', false);
 defined('YII_ENV') or define('YII_ENV', 'prod');
+defined('HEROKU_APP') or define('HEROKU_APP', getenv('HEROKU_APP'));
 
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
-$dotenv = new \Dotenv\Dotenv(dirname(__DIR__));
-$dotenv->load();
+if (!HEROKU_APP) {
+    $dotenv = new \Dotenv\Dotenv(dirname(__DIR__));
+    $dotenv->load();
+}
 
 $config = require(__DIR__ . '/../config/web.php');
 
